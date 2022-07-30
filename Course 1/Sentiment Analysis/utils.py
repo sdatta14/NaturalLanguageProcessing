2d7a77c677654af1b5bdfdb3eceb418e81478906
sdatta14 <98547174+sdatta14@users.noreply.github.com>
2022-07-30 12:25:30 +0530

    Update and rename Course 1/Week 1/utils.py to Course 1/Sentiment Analysis/utils.py

--- Course 1/Sentiment Analysis/utils.py
+++ Course 1/Sentiment Analysis/utils.py
@@ -71,3 +71,20 @@ def build_freqs(tweets, ys):
                 freqs[pair] = 1
 
     return freqs
+
+def lookup(freqs, word, label):
+    '''
+    Input:
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        word: the word to look up
+        label: the label corresponding to the word
+    Output:
+        n: the number of times the word with its corresponding label appears.
+    '''
+    n = 0  # freqs.get((word, label), 0)
+
+    pair = (word, label)
+    if (pair in freqs):
+        n = freqs[pair]
+
+    return n
